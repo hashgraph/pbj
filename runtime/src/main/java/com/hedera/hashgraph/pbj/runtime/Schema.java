523485b1f3206d6ebce886bf134dc7e06bf96d00
jasperpotts <jasperpotts@users.noreply.github.com>
2022-11-10 08:48:31 -0800

    Cleanup of schema
    Signed-off-by: jasperpotts <jasperpotts@users.noreply.github.com>

--- runtime/src/main/java/com/hedera/hashgraph/pbj/runtime/Schema.java
+++ runtime/src/main/java/com/hedera/hashgraph/pbj/runtime/Schema.java
@@ -2,22 +2,12 @@ package com.hedera.hashgraph.pbj.runtime;
 
 /**
  * Interface for Schemas, schemas are a programmatic model of protobuf schema. Used in parsing, writing protobuf,
- * to/from record objects.
+ * to/from record objects. It is just a marker interface as all methods are static. Implementing classes are expected to
+ * provide static methods with the following signatures:
+ *<ul>
+ *     <li><code>public static boolean valid(FieldDefinition f) {...}</code></li>
+ *     <li><code>public static FieldDefinition getField(final int fieldNumber) {...}</code></li>
+ *</ul>
  */
 public interface Schema {
-//	/**
-//	 * Check if a field definition belongs to this schema.
-//	 *
-//	 * @param f field def to check
-//	 * @return true if it belongs to this schema
-//	 */
-//	boolean valid(FieldDefinition f);
-//
-//	/**
-//	 * Get a field definition given a field number
-//	 *
-//	 * @param fieldNumber the fields number to get def for
-//	 * @return field def or null if field number does not exist
-//	 */
-//	FieldDefinition getFieldDefinition(final int fieldNumber);
 }
