df122f847fdf778e25b41443ee850bafe919a29e
Lubomir Litchev <lubomir@swirldslabs.com>
2023-05-18 17:10:57 -0700

    Make the new Exception class a subclass of BufferUnderFlowException.
    Signed-off-by: Lubomir Litchev <lubomir@swirldslabs.com>

--- pbj-core/pbj-runtime/src/main/java/com/hedera/pbj/runtime/io/stream/EOFException.java
+++ pbj-core/pbj-runtime/src/main/java/com/hedera/pbj/runtime/io/stream/EOFException.java
@@ -1,8 +1,10 @@
 package com.hedera.pbj.runtime.io.stream;
 
+import java.nio.BufferUnderflowException;
+
 /** This class is used as an exception to signal that the end of stream is reached
  * when reading.
  */
-public class EOFException  extends RuntimeException {
+public class EOFException  extends BufferUnderflowException {
     private static final long serialVersionUID = 1799983599892333203L;
 }
