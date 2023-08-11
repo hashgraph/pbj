7a621af7e4c9a7424527d830cd182db37dcd2b80
Richard Bair <rbair23@users.noreply.github.com>
2023-08-11 16:05:23 -0700

    Use array offset when needed to work with views. Add many tests.
    Signed-off-by: Richard Bair <rbair23@users.noreply.github.com>

--- pbj-core/pbj-runtime/src/test/java/com/hedera/pbj/runtime/io/buffer/BufferedDataTest.java
+++ pbj-core/pbj-runtime/src/test/java/com/hedera/pbj/runtime/io/buffer/BufferedDataTest.java
@@ -11,8 +11,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 final class BufferedDataTest {
-    // TODO Test that "view" shows the updated data when it is changed on the fly
-    // TODO Verify capacity is never negative (it can't be, maybe nothing to test here)
+    // FUTURE Test that "view" shows the updated data when it is changed on the fly
+    // FUTURE Verify capacity is never negative (it can't be, maybe nothing to test here)
 
     @Test
     @DisplayName("toString() is safe")
@@ -87,6 +87,113 @@ final class BufferedDataTest {
         @Override
         protected WritableSequentialData eofSequence() {
             return BufferedData.wrap(new byte[0]);
+        }
+
+        @NonNull
+        @Override
+        protected byte[] extractWrittenBytes(@NonNull WritableSequentialData seq) {
+            final var buf = (BufferedData) seq;
+            final var bytes = new byte[Math.toIntExact(buf.position())];
+            buf.getBytes(0, bytes);
+            return bytes;
+        }
+    }
+
+    @Nested
+    final class ReadableSequentialDataViewTest extends ReadableTestBase {
+
+        @NonNull
+        @Override
+        protected ReadableSequentialData emptySequence() {
+            final var buf = BufferedData.wrap(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            buf.position(7);
+            return buf.view(0);
+        }
+
+        @NonNull
+        @Override
+        protected ReadableSequentialData fullyUsedSequence() {
+            final var buf = BufferedData.wrap(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            buf.position(2);
+            final var view = buf.view(5);
+            view.skip(5);
+            return view;
+        }
+
+        @NonNull
+        @Override
+        protected ReadableSequentialData sequence(@NonNull byte[] arr) {
+            BufferedData buf = BufferedData.allocate(arr.length + 20);
+            for (int i = 0; i < 10; i++) {
+                buf.writeByte((byte) i);
+            }
+            buf.position(buf.length() - 10);
+            for (int i = 0; i < 10; i++) {
+                buf.writeByte((byte) i);
+            }
+            buf.position(10);
+            buf.writeBytes(arr);
+            buf.position(10);
+            return buf.view(arr.length);
+        }
+    }
+
+    @Nested
+    final class RandomAccessDataViewTest extends ReadableTestBase {
+
+        @NonNull
+        @Override
+        protected ReadableSequentialData emptySequence() {
+            final var buf = BufferedData.wrap(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            buf.position(7);
+            return new RandomAccessSequenceAdapter(buf.view(0));
+        }
+
+        @NonNull
+        @Override
+        protected ReadableSequentialData fullyUsedSequence() {
+            final var buf = BufferedData.wrap(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            buf.position(2);
+            final var view = buf.view(5);
+            view.skip(5);
+            return new RandomAccessSequenceAdapter(view, 5);
+        }
+
+        @NonNull
+        @Override
+        protected ReadableSequentialData sequence(@NonNull byte[] arr) {
+            BufferedData buf = BufferedData.allocate(arr.length + 20);
+            for (int i = 0; i < 10; i++) {
+                buf.writeByte((byte) i);
+            }
+            buf.position(buf.length() - 10);
+            for (int i = 0; i < 10; i++) {
+                buf.writeByte((byte) i);
+            }
+            buf.position(10);
+            buf.writeBytes(arr);
+            buf.position(10);
+            return new RandomAccessSequenceAdapter(buf.view(arr.length));
+        }
+    }
+
+    @Nested
+    final class WritableSequentialDataViewTest extends WritableTestBase {
+        @NonNull
+        @Override
+        protected WritableSequentialData sequence() {
+            final var mb = 1024 * 1024;
+            final var buf = BufferedData.allocate(mb * 4); // the largest expected test value is 1 mb
+            buf.position(mb);
+            return buf.view(mb * 2);
+        }
+
+        @NonNull
+        @Override
+        protected WritableSequentialData eofSequence() {
+            final var buf = BufferedData.wrap(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            buf.position(7);
+            return buf.view(0);
         }
 
         @NonNull
