88c8dcab3446ddecb0b9d3333b30b926cd2c41d3
Anthony Petrov <anthony@swirldslabs.com>
2023-12-11 11:30:46 -0800

    fix 68: introduce FuzzTest
    Signed-off-by: Anthony Petrov <anthony@swirldslabs.com>

---
+++ pbj-integration-tests/src/main/java/com/hedera/pbj/integration/fuzz/FuzzTestResult.java
@@ -0,0 +1,36 @@
+package com.hedera.pbj.integration.fuzz;
+
+import java.text.NumberFormat;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * A record that describes the result of running a fuzz test.
+ * @param object an object for which this test was run.
+ * @param passed indicates if the test passed or not. See the FuzzTest class for the definition.
+ * @param percentageMap a map with percentage statistics of occurred outcomes.
+ * @param <T> the type of the object for which the test was run
+ */
+public record FuzzTestResult<T>(
+        T object,
+        boolean passed,
+        Map<SingleFuzzTestResult, Double> percentageMap
+) {
+    private static final NumberFormat PERCENTAGE_FORMAT = NumberFormat.getPercentInstance();
+
+    /**
+     * Format the FuzzTestResult object for printing/logging.
+     */
+    public String format() {
+        return "A fuzz test " + (passed ? "PASSED" : "FAILED") + " for "
+                + object + " with:" + System.lineSeparator()
+                + formatResultsStats();
+    }
+
+    private String formatResultsStats() {
+        return percentageMap.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .map(entry -> entry.getKey().name() + ": " + PERCENTAGE_FORMAT.format(entry.getValue()))
+                .collect(Collectors.joining(System.lineSeparator()));
+    }
+}
--- pbj-integration-tests/src/test/java/com/hedera/pbj/intergration/test/SampleFuzzTest.java
+++ pbj-integration-tests/src/test/java/com/hedera/pbj/intergration/test/SampleFuzzTest.java
@@ -1,35 +1,32 @@
 package com.hedera.pbj.intergration.test;
 
 import com.hedera.hapi.node.base.AccountID;
-import com.hedera.pbj.integration.fuzz.SingleFuzzTest;
-import com.hedera.pbj.integration.fuzz.SingleFuzzTestResult;
-import java.util.Random;
+import com.hedera.pbj.integration.fuzz.FuzzTest;
+import com.hedera.pbj.integration.fuzz.FuzzTestResult;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
- * This is a sample fuzz test just to demonstrate the usage of the SingleFuzzTest class.
- * It's unable to assert or verify anything because a single fuzz test run may not
- * produce a desired result.
+ * This is a sample fuzz test just to demonstrate the usage of the FuzzTest class.
  * It will be replaced with a more elaborate fuzz testing framework in the future.
- * See javadoc for SingleFuzzTest for more details.
+ * See javadoc for FuzzTest for more details.
  */
 public class SampleFuzzTest {
 
     @Test
     void testMethod() {
         AccountID accountID = AccountID.newBuilder().accountNum(1).realmNum(2).shardNum(3).build();
-        SingleFuzzTestResult singleFuzzTestResult = SingleFuzzTest.fuzzTest(
-                accountID,
-                AccountID.PROTOBUF,
-                new Random()
-        );
-        System.out.println("A fuzz test for " + accountID + " resulted in " + singleFuzzTestResult);
 
-        // It's a no-op by design currently. See javadoc for details.
-        assertNotNull(singleFuzzTestResult);
+        // A percentage threshold for the DESERIALIZATION_FAILED outcomes.
+        final double THRESHOLD = 0.5;
+
+        FuzzTestResult<AccountID> fuzzTestResult = FuzzTest.fuzzTest(accountID, AccountID.PROTOBUF, THRESHOLD);
+
+        System.out.println(fuzzTestResult.format());
+
+        assertTrue(fuzzTestResult.passed());
     }
 
 }
