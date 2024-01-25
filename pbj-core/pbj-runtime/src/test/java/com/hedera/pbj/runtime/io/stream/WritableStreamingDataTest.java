290bdfa759bc54b002944b208d1de82d03d296d5
Anthony Petrov <anthony@swirldslabs.com>
2024-01-25 11:28:19 -0800

    address Codacy
    Signed-off-by: Anthony Petrov <anthony@swirldslabs.com>

--- pbj-core/pbj-runtime/src/test/java/com/hedera/pbj/runtime/io/stream/WritableStreamingDataTest.java
+++ pbj-core/pbj-runtime/src/test/java/com/hedera/pbj/runtime/io/stream/WritableStreamingDataTest.java
@@ -23,7 +23,6 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
 
 public class WritableStreamingDataTest extends WritableTestBase {
 
