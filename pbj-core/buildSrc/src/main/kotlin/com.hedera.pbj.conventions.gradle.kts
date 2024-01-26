fd1ad4223cd8514709781df08297699a7218bc26
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-01-26 09:03:09 +0100

    chore: remove duplicated Java version settings
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

--- pbj-core/buildSrc/src/main/kotlin/com.hedera.pbj.conventions.gradle.kts
+++ pbj-core/buildSrc/src/main/kotlin/com.hedera.pbj.conventions.gradle.kts
@@ -44,9 +44,6 @@ javaModuleDependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
         vendor.set(JvmVendorSpec.ADOPTIUM)
