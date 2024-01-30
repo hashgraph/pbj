91b2b3420dd0b0d747dacbb496729c4663f5d9d4
Anthony Petrov <anthony@swirldslabs.com>
2024-01-29 16:53:14 -0800

    add missing import
    Signed-off-by: Anthony Petrov <anthony@swirldslabs.com>

--- pbj-core/pbj-runtime/src/main/java/com/hedera/pbj/runtime/io/SequentialData.java
+++ pbj-core/pbj-runtime/src/main/java/com/hedera/pbj/runtime/io/SequentialData.java
@@ -1,5 +1,7 @@
 package com.hedera.pbj.runtime.io;
 
+import java.io.UncheckedIOException;
+
 /**
  * Represents sequential data which may either be buffered or streamed. Conceptually, streamed data is
  * a sequential stream of bytes, while a buffer is a sequential array of bytes. A stream <b>necessarily</b>
