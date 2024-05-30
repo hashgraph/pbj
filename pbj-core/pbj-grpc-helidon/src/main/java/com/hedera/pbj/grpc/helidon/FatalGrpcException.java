67fd8453ffe758e4e3e7888711762b60c762c679
Richard Bair <rbair23@users.noreply.github.com>
2024-05-30 13:45:37 -0700

    WIP: writing tests
    Signed-off-by: Richard Bair <rbair23@users.noreply.github.com>

--- pbj-core/pbj-grpc-helidon/src/main/java/com/hedera/pbj/grpc/helidon/FatalGrpcException.java
+++ pbj-core/pbj-grpc-helidon/src/main/java/com/hedera/pbj/grpc/helidon/FatalGrpcException.java
@@ -1,20 +1,35 @@
 package com.hedera.pbj.grpc.helidon;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
+import io.helidon.http.Header;
 import io.helidon.http.Status;
 import io.helidon.http.WritableHeaders;
 import io.helidon.http.http2.Http2Headers;
 import java.util.function.Consumer;
 
 class FatalGrpcException extends Exception {
-    final transient Consumer<WritableHeaders<?>> headerCallback;
+    final Consumer<WritableHeaders<?>> headerCallback;
 
     FatalGrpcException(final @NonNull Consumer<WritableHeaders<?>> headerCallback) {
         this.headerCallback = headerCallback;
     }
 
+    FatalGrpcException(final @NonNull Header grpcStatus) {
+        this(w -> {
+            w.set(Http2Headers.STATUS_NAME, Status.OK_200.code());
+            w.set(grpcStatus);
+        });
+    }
+
     FatalGrpcException(final @NonNull Status status) {
         this(w -> w.set(Http2Headers.STATUS_NAME, status.code()));
+    }
+
+    FatalGrpcException(final @NonNull Status status, final @NonNull Header grpcStatus) {
+        this(w -> {
+            w.set(Http2Headers.STATUS_NAME, status.code());
+            w.set(grpcStatus);
+        });
     }
 
     final Consumer<WritableHeaders<?>> headerCallback() {
