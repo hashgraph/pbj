ead8e5ffab47c89a4043b1b1c8b37ab7f69acfe9
Richard Bair <rbair23@users.noreply.github.com>
2024-06-01 12:40:43 -0700

    Rewritten and simplified for concise and reasonable performance of the Service Interface.
    New Pipelines class with all the Flow.* work and a fluent API for
    building out the handling pipeline.
    
    Signed-off-by: Richard Bair <rbair23@users.noreply.github.com>

--- pbj-core/pbj-runtime/src/main/java/com/hedera/pbj/runtime/grpc/ServiceInterface.java
+++ pbj-core/pbj-runtime/src/main/java/com/hedera/pbj/runtime/grpc/ServiceInterface.java
@@ -84,6 +84,7 @@ public interface ServiceInterface {
      */
     @NonNull
     Flow.Subscriber<? super Bytes> open(
+            @NonNull RequestOptions opts,
             @NonNull Method method,
             @NonNull RequestOptions opts,
             @NonNull Flow.Subscriber<? super Bytes> responses) throws GrpcException;
