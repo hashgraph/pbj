8a002b06071f827cc7c9c5057f41ad3e814c9ed2
Richard Bair <rbair23@users.noreply.github.com>
2024-06-27 09:04:37 -0700

    Add toolchain resolver
    Signed-off-by: Richard Bair <rbair23@users.noreply.github.com>

--- pbj-core/settings.gradle.kts
+++ pbj-core/settings.gradle.kts
@@ -18,6 +18,7 @@ pluginManagement { includeBuild("gradle/plugins") }
 
 plugins {
     id("com.gradle.enterprise").version("3.15.1")
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
 include(":pbj-runtime")
