3c093031add0d474d90227c6e452aed411038044
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-10-09 11:15:37 +0200

    build: use plugins from 'hiero-gradle-conventions'
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

---
+++ pbj-core/pbj-compiler/src/main/java/com/hedera/pbj/compiler/PbjCompiler.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2023-2024 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hedera.pbj.compiler;
+
+import com.hedera.pbj.compiler.impl.ContextualLookupHelper;
+import com.hedera.pbj.compiler.impl.LookupHelper;
+import com.hedera.pbj.compiler.impl.generators.EnumGenerator;
+import com.hedera.pbj.compiler.impl.generators.Generator;
+import com.hedera.pbj.compiler.impl.grammar.Protobuf3Lexer;
+import com.hedera.pbj.compiler.impl.grammar.Protobuf3Parser;
+import java.io.File;
+import java.io.FileInputStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+/** Gradle Task that generates java src code from protobuf proto schema files. */
+public abstract class PbjCompiler {
+    private static final int MAX_TRACE_FRAMES = 8;
+    private static final String STACK_ELEMENT_INDENT = "    ";
+
+    public static void compileFilesIn(Iterable<File> sourceFiles, File mainOutputDir, File testOutputDir)
+            throws Exception {
+        // first we do a scan of files to build lookup tables for imports, packages etc.
+        final LookupHelper lookupHelper = new LookupHelper(sourceFiles);
+        // for each proto src directory generate code
+        for (final File protoFile : sourceFiles) {
+            if (protoFile.exists()
+                    && protoFile.isFile()
+                    && protoFile.getName().endsWith(LookupHelper.PROTO_EXTENSIION)) {
+                final ContextualLookupHelper contextualLookupHelper =
+                        new ContextualLookupHelper(lookupHelper, protoFile);
+                try (final var input = new FileInputStream(protoFile)) {
+                    final var lexer = new Protobuf3Lexer(CharStreams.fromStream(input));
+                    final var parser = new Protobuf3Parser(new CommonTokenStream(lexer));
+                    final Protobuf3Parser.ProtoContext parsedDoc = parser.proto();
+                    for (final var topLevelDef : parsedDoc.topLevelDef()) {
+                        final Protobuf3Parser.MessageDefContext msgDef = topLevelDef.messageDef();
+                        if (msgDef != null) {
+                            // run all generators for message
+                            for (final var generatorClass : Generator.GENERATORS) {
+                                final var generator =
+                                        generatorClass.getDeclaredConstructor().newInstance();
+                                generator.generate(msgDef, mainOutputDir, testOutputDir, contextualLookupHelper);
+                            }
+                        }
+                        final Protobuf3Parser.EnumDefContext enumDef = topLevelDef.enumDef();
+                        if (enumDef != null) {
+                            // run just enum generators for enum
+                            EnumGenerator.generateEnumFile(enumDef, mainOutputDir, contextualLookupHelper);
+                        }
+                    }
+                } catch (Exception e) {
+                    System.err.println("Exception while processing file: " + protoFile);
+                    // Print an abbreviated stack trace for help in debugging.
+                    System.err.println(e);
+                    var trace = e.getStackTrace();
+                    int count = 0;
+                    for (var element : trace) {
+                        if (count++ < MAX_TRACE_FRAMES) System.err.println(STACK_ELEMENT_INDENT + element);
+                    }
+                    throw e;
+                }
+            }
+        }
+    }
+}
--- pbj-core/settings.gradle.kts
+++ pbj-core/settings.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022-2023 Hedera Hashgraph, LLC
+ * Copyright (C) 2022-2024 Hedera Hashgraph, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,87 +14,11 @@
  * limitations under the License.
  */
 
-pluginManagement { includeBuild("gradle/plugins") }
+plugins { id("org.hiero.gradle.build") version "0.2.0" }
 
-plugins {
-    id("com.gradle.enterprise").version("3.15.1")
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
-
-include(":pbj-runtime")
-include(":pbj-compiler")
-include(":pbj-grpc-helidon-config")
-include(":pbj-grpc-helidon")
-
-gradleEnterprise {
-    buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
-    }
-}
-
-var helidonVersion = "4.1.1"
-var grpcVersion = "1.65.1"
-var protobufVersion = "4.28.2"
-
-dependencyResolutionManagement {
-    versionCatalogs {
-        create("libs") {
-            // The libs of this catalog are the **ONLY** ones that are authorized to be part of the runtime
-            // distribution. These libs can be depended on during compilation, or bundled as part of runtime.
-            version("org.antlr.antlr4.runtime", "4.11.1")
-            version("com.github.spotbugs.annotations", "4.8.6")
-
-            // The libs of this catalog are used to compile but are not bundled. The user will select helidon
-            // which will have the set of these dependencies that are required.
-            version("io.helidon.webserver", helidonVersion)
-            version("io.helidon.webserver.http2", helidonVersion)
-            version("io.helidon.webserver.observe.metrics", helidonVersion)
-            version("io.helidon.metrics.api", helidonVersion)
-            version("io.helidon.common.features.api", helidonVersion)
-            version("io.helidon.common.features.processor", helidonVersion) // annotation processing
-            version("io.helidon.config.metadata.processor", helidonVersion) // annotation processing
-            version("io.helidon.codegen.apt", helidonVersion) // annotation processing
-            version("io.helidon.builder.codegen", helidonVersion) // annotation processing
-
-            // Testing only versions
-            version("com.google.protobuf", protobufVersion)
-            version("com.google.protobuf.util", protobufVersion)
-            version("org.assertj.core", "3.23.1")
-            version("org.junit.jupiter.api", "5.8.2")
-            version("org.mockito", "4.6.1")
-            version("org.mockito.inline", "4.6.1")
-            version("org.mockito.junit.jupiter", "5.10.0")
-            version("io.grpc.netty", grpcVersion)
-            version("io.grpc.protobuf", grpcVersion)
-            version("io.grpc.stub", grpcVersion)
-            version("java.annotation", "1.3.2")
-            version("io.helidon.webclient", helidonVersion)
-            version("io.helidon.webclient.http2", helidonVersion)
-        }
-    }
-}
-
-// Build cache configuration
-val isCiServer = System.getenv().containsKey("CI")
-val gradleCacheUsername: String? = System.getenv("GRADLE_CACHE_USERNAME")
-val gradleCachePassword: String? = System.getenv("GRADLE_CACHE_PASSWORD")
-val gradleCacheAuthorized =
-    (gradleCacheUsername?.isNotEmpty() ?: false) && (gradleCachePassword?.isNotEmpty() ?: false)
-
-buildCache {
-    remote<HttpBuildCache> {
-        url = uri("https://cache.gradle.hedera.svcs.eng.swirldslabs.io/cache/")
-        isPush = isCiServer && gradleCacheAuthorized
-
-        isUseExpectContinue = true
-        isEnabled = !gradle.startParameter.isOffline
-
-        if (isCiServer && gradleCacheAuthorized) {
-            credentials {
-                username = gradleCacheUsername
-                password = gradleCachePassword
-            }
-        }
+javaModules {
+    directory(".") {
+        group = "com.hedera.pbj"
+        module("pbj-compiler") // no 'module-info.java'
     }
 }
