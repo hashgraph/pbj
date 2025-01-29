07e78b4b0ac2309c71de31164eb5b69073ee912b
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-01-29 12:41:06 +0100

    build: Downgrade dependency-analysis-gradle-plugin to 2.6.0
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

--- pbj-integration-tests/settings.gradle.kts
+++ pbj-integration-tests/settings.gradle.kts
@@ -5,6 +5,14 @@ pluginManagement {
 
 plugins { id("org.hiero.gradle.build") version "0.3.1" }
 
+// Downgrade 'dependency-analysis-gradle-plugin' due to regression in 2.7.0
+// https://github.com/autonomousapps/dependency-analysis-gradle-plugin/issues/1364
+buildscript {
+    dependencies.constraints {
+        classpath("com.autonomousapps:dependency-analysis-gradle-plugin:2.6.0!!")
+    }
+}
+
 dependencyResolutionManagement {
     // To use locally built 'pbj-runtime'
     includeBuild("../pbj-core")
