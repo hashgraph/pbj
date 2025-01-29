hashgraph/pbj

commit 2fbee2bb812903dec6d607f9c16ce212e8a9ee26

build: use common Hiero Gradle config (#302)
Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

.github/workflows/flow-deploy-release-artifact.yaml
@@ -150,22 +150,21 @@ jobs:
         working-directory: ${{ env.PBJ_CORE }}
         run: ./gradlew assemble --scan
 
-      - name: Gradle Maven Central Release
+      - name: Gradle Release Maven Central
+        if: ${{ !cancelled() && !failure() }}
+        working-directory: ${{ env.PBJ_CORE }}
+        env:
+          NEXUS_USERNAME: ${{ secrets.OSSRH_USERNAME }}
+          NEXUS_PASSWORD: ${{ secrets.OSSRH_PASSWORD }}
+        # The 'releaseMavenCentral' task will select if Release or Snapshot based on the version
+        run: |
+          ./gradlew releaseMavenCentral -PpublishingPackageGroup=com.hedera.pbj -PpublishSigningEnabled=true --no-configuration-cache --scan
+
+      - name: Gradle Plugin Portal Release
         if: ${{ needs.prepare-release.outputs.mode == 'specified' && !cancelled() && !failure() }}
         working-directory: ${{ env.PBJ_CORE }}
         env:
-          OSSRH_USERNAME: ${{ secrets.OSSRH_USERNAME }}
-          OSSRH_PASSWORD: ${{ secrets.OSSRH_PASSWORD }}
           GRADLE_PUBLISH_KEY: ${{ secrets.GRADLE_PUBLISH_KEY }}
           GRADLE_PUBLISH_SECRET: ${{ secrets.GRADLE_PUBLISH_SECRET }}
         run: |
-          ./gradlew release --no-parallel --scan -PpublishSigningEnabled=true \
-              -Pgradle.publish.key=${GRADLE_PUBLISH_KEY} -Pgradle.publish.secret=${GRADLE_PUBLISH_SECRET}
-
-      - name: Gradle Maven Central Snapshot
-        if: ${{ needs.prepare-release.outputs.mode == 'snapshot' && !cancelled() && !failure() }}
-        working-directory: ${{ env.PBJ_CORE }}
-        env:
-          OSSRH_USERNAME: ${{ secrets.OSSRH_USERNAME }}
-          OSSRH_PASSWORD: ${{ secrets.OSSRH_PASSWORD }}
-        run: ./gradlew releaseSnapshot --no-parallel --scan -PpublishSigningEnabled=true
+          ./gradlew publishPlugins -PpublishSigningEnabled=true --no-configuration-cache --scan

.github/workflows/zxc-compile-pbj-code.yaml
@@ -126,15 +126,6 @@ jobs:
 #          project-token: ${{ secrets.codacy-project-token }}
 #          coverage-reports: ${{ env.PBJ_CORE }}/pbj-compiler/build/reports/jacoco/test/jacocoTestReport.xml, ${{ env.PBJ_CORE }}/pbj-runtime/build/reports/jacoco/test/jacocoTestReport.xml
 
-      - name: Gradle Maven Local (PBJ Core)
-        id: gradle-publish-local
-        uses: gradle/actions/setup-gradle@dbbdc275be76ac10734476cc723d82dfe7ec6eda # v3.4.2
-        if: ${{ (inputs.enable-integration-tests || inputs.enable-jmh-tests) && steps.gradle-build.conclusion == 'success' && !cancelled() }}
-        with:
-          gradle-version: ${{ inputs.gradle-version }}
-          build-root-directory: ${{ env.PBJ_CORE }}
-          arguments: publishToMavenLocal --scan
-
       - name: Gradle Assemble (PBJ Integration)
         id: gradle-integration-build
         uses: gradle/actions/setup-gradle@dbbdc275be76ac10734476cc723d82dfe7ec6eda # v3.4.2

pbj-integration-tests/build.gradle.kts
@@ -1,64 +1,107 @@
+/*
+ * Copyright (C) 2025 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.hiero.gradle.tasks.GitClone
+
 plugins {
-    id("java")
-    id("jacoco")
+    id("org.hiero.gradle.module.application")
+    id("org.gradlex.java-module-dependencies")
+    // jmh for performance benchmarks
+    id("org.hiero.gradle.feature.benchmark")
+    // We depend on Google protobuf plugin as we generate protobuf code using it as well as pbj.
+    // Then use it in tests to compare output and parsing with pbj to make sure it matches.
+    id("org.hiero.gradle.feature.protobuf")
+    // The "plugin-under-test"
     id("com.hedera.pbj.pbj-compiler")
-    // We depend on Google protobuf plugin as we generate protobuf code using it as well as pbj. Then use it in tests to
-    // compare output and parsing with pbj to make sure it matches.
-    id("com.google.protobuf").version("0.9.4")
-    // add jmh for performance benchmarks
-    id("me.champeau.jmh").version("0.7.2")
 }
 
-group = "com.hedera.pbj.integration-tests"
-
-dependencies {
-    implementation("com.hedera.pbj:pbj-runtime")
-    implementation("com.hedera.pbj:pbj-compiler")
-    implementation("com.google.protobuf:protobuf-java:4.28.2")
-    implementation("com.google.protobuf:protobuf-java-util:4.28.2")
-    implementation("io.grpc:grpc-protobuf:1.65.1")
-    implementation("io.grpc:grpc-stub:1.65.1")
-    implementation("javax.annotation:javax.annotation-api:1.3.2")
-    compileOnly("com.github.spotbugs:spotbugs-annotations:4.7.3")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-params")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+// We use the dependency versions from the included main build, which we need to configure here
+buildscript {
+    dependencies { classpath(platform("com.hedera.hashgraph:hiero-dependency-versions")) }
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
-        vendor.set(JvmVendorSpec.ADOPTIUM)
-    }
-
-    // Enable JAR file generation required for publishing
-    withJavadocJar()
-    withSourcesJar()
+jvmDependencyConflicts.consistentResolution {
+    platform("com.hedera.hashgraph:hiero-dependency-versions")
 }
+
+mainModuleInfo {
+    requires("com.hedera.pbj.runtime")
+
+    requires("com.google.common")
+    requires("com.google.protobuf")
+    requires("com.google.protobuf.util")
+    requires("io.grpc")
+    requires("io.grpc.protobuf")
+    requires("io.grpc.stub")
+    requires("org.antlr.antlr4.runtime")
+    requiresStatic("com.github.spotbugs.annotations")
+}
+
+testModuleInfo {
+    requires("org.junit.jupiter.api")
+    requires("org.junit.jupiter.params")
+    runtimeOnly("org.junit.jupiter.engine")
+}
+
+jmhModuleInfo { requires("com.hedera.pbj.runtime") }
+
+// IMPROVE: Disable module-info transform for 'testRuntimeClasspath' which leads to an error
+// possible caused by a cycle produced by depending on 'pbj-compiler' in multiple ways which
+// eventually leads to 'pbj-compiler' depending on itself in this context.
+configurations.testRuntimeClasspath {
+    attributes { attribute(Attribute.of("javaModule", Boolean::class.javaObjectType), false) }
+}
+
+// IMPROVE: Test code should not have a direct dependency to 'com.hedera.pbj.compiler'
+dependencies { testImplementation("com.hedera.pbj:pbj-compiler") { isTransitive = false } }
+
+dependencyAnalysis { issues { all { onAny { exclude("com.hedera.pbj:pbj-compiler") } } } }
+
+// IMPROVE: JMH code should not depend on test code
+jmh { includeTests = true }
 
 // Add downloaded HAPI repo protobuf files into build directory and add to sources to build them
+val cloneHederaProtobufs =
+    tasks.register<GitClone>("cloneHederaProtobufs") {
+        localCloneDirectory = layout.buildDirectory.dir("hedera-protobufs")
+        url = "https://github.com/hashgraph/hedera-protobufs.git"
+        // choose tag or branch of HAPI you would like to test with
+        // branch = "main"
+        tag = "v0.55.0"
+    }
+
 sourceSets {
     main {
         pbj {
-            srcDir(layout.buildDirectory.dir("repos/hapi/block"))
-            srcDir(layout.buildDirectory.dir("repos/hapi/services"))
-            srcDir(layout.buildDirectory.dir("repos/hapi/streams"))
-            srcDir(layout.buildDirectory.dir("repos/hapi/platform"))
+            srcDir(cloneHederaProtobufs.flatMap { it.localCloneDirectory.dir("block") })
+            srcDir(cloneHederaProtobufs.flatMap { it.localCloneDirectory.dir("platform") })
+            srcDir(cloneHederaProtobufs.flatMap { it.localCloneDirectory.dir("services") })
+            srcDir(cloneHederaProtobufs.flatMap { it.localCloneDirectory.dir("streams") })
         }
         proto {
-            srcDir(layout.buildDirectory.dir("repos/hapi/block"))
-            srcDir(layout.buildDirectory.dir("repos/hapi/services"))
-            srcDir(layout.buildDirectory.dir("repos/hapi/streams"))
-            srcDir(layout.buildDirectory.dir("repos/hapi/platform"))
+            srcDir(cloneHederaProtobufs.flatMap { it.localCloneDirectory.dir("block") })
+            srcDir(cloneHederaProtobufs.flatMap { it.localCloneDirectory.dir("platform") })
+            srcDir(cloneHederaProtobufs.flatMap { it.localCloneDirectory.dir("services") })
+            srcDir(cloneHederaProtobufs.flatMap { it.localCloneDirectory.dir("streams") })
         }
     }
 }
 
-/** Exclude protoc generated from docs, so we can see clear warnings and errors */
-tasks.withType<Javadoc> {
+// Exclude protoc generated from docs
+tasks.javadoc {
     exclude("com/hederahashgraph/api/proto/**")
     exclude("com/hederahashgraph/service/proto/**")
     exclude("com/hedera/hapi/**/legacy/**")
@@ -69,105 +112,39 @@ tasks.withType<Javadoc> {
     exclude("pbj/**")
 }
 
-protobuf {
-    // Configure the protoc executable
-    protoc {
-        // Download from repositories
-        artifact = "com.google.protobuf:protoc:4.28.2"
-    }
-    plugins {
-        create("grpc") {
-            artifact = "io.grpc:protoc-gen-grpc-java:1.65.1"
-        }
-    }
-
-    generateProtoTasks {
-        all().forEach {
-            it.plugins {
-                create("grpc")
-            }
-        }
-    }
-}
-
 testing {
     @Suppress("UnstableApiUsage")
-    suites.getByName<JvmTestSuite>("test") {
-        useJUnitJupiter()
-
+    suites.named<JvmTestSuite>("test") {
         tasks.register<Test>("fuzzTest") {
             testClassesDirs = sources.output.classesDirs
             classpath = sources.runtimeClasspath
-
             useJUnitPlatform { includeTags("FUZZ_TEST") }
             enableAssertions = false
         }
-
         tasks.register<Test>("randomFuzzTest") {
             testClassesDirs = sources.output.classesDirs
             classpath = sources.runtimeClasspath
-
             useJUnitPlatform { includeTags("FUZZ_TEST") }
             enableAssertions = false
             systemProperties["com.hedera.pbj.intergration.test.fuzz.useRandomSeed"] = true
         }
+        targets.named("test") {
+            testTask {
+                useJUnitPlatform { excludeTags("FUZZ_TEST") }
+                dependsOn(tasks.named("fuzzTest"))
+            }
+        }
     }
-}
-
-tasks.test {
-    useJUnitPlatform { excludeTags("FUZZ_TEST") }
-    dependsOn("fuzzTest")
 }
 
 tasks.withType<Test>().configureEach {
     // We are running a lot of tests 10s of thousands, so they need to run in parallel
     systemProperties["junit.jupiter.execution.parallel.enabled"] = true
     systemProperties["junit.jupiter.execution.parallel.mode.default"] = "concurrent"
-    // us parallel GC to keep up with high temporary garbage creation, and allow GC to use 40% of CPU if needed
+    // us parallel GC to keep up with high temporary garbage creation, and allow GC to use 40% of
+    // CPU if needed
     jvmArgs("-XX:+UseParallelGC", "-XX:GCTimeRatio=90")
     // Some also need more memory
     minHeapSize = "512m"
     maxHeapSize = "4096m"
-}
-
-jmh {
-    includes.add("SampleBlockBench")
-//    includes.add("JsonBench")
-//    includes.add("VarIntBench")
-//    includes.add("HashBench")
-//    includes.add("EqualsHashCodeBench");
-
-    jmhVersion.set("1.37")
-    includeTests.set(true)
-//    jvmArgsAppend.add("-XX:MaxInlineSize=100 -XX:MaxInlineLevel=20")
-}
-
-tasks.jmhJar {
-    manifest {
-        attributes(mapOf("Multi-Release" to true))
-    }
-    // I've no ideas why these classes are in the shadow jar, they seem redundant. If
-    // not excluded, they make jmhJar fail, complaining about too many jar entries
-    exclude("org/gradle/**")
-    exclude("groovy*/**")
-    exclude("**/groovy/**")
-    exclude("kotlin/**")
-}
-
-// Ensure JaCoCo coverage is generated and aggregated
-tasks.jacocoTestReport.configure {
-    reports {
-        xml.required.set(true)
-        html.required.set(true)
-    }
-
-    val testExtension = tasks.test.get().extensions.getByType<JacocoTaskExtension>()
-    executionData.setFrom(testExtension.destinationFile)
-
-    shouldRunAfter(tasks.named("check"))
-}
-
-// Ensure the check task also runs the JaCoCo coverage report
-tasks.named("check").configure {
-    dependsOn(tasks.named<JacocoReport>("jacocoTestReport"))
 }

pbj-core/build.gradle.kts
@@ -1,19 +1,0 @@
-/*
- * Copyright (C) 2022-2023 Hedera Hashgraph, LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-plugins {
-    id("com.hedera.pbj.root")
-}

pbj-core/gradle/aggregation/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022-2024 Hedera Hashgraph, LLC
+ * Copyright (C) 2024-2025 Hedera Hashgraph, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,10 +15,10 @@
  */
 
 plugins {
-    id("java-library")
-    id("com.hedera.pbj.conventions")
+    id("org.hiero.gradle.base.lifecycle")
+    id("org.hiero.gradle.report.code-coverage")
+    id("org.hiero.gradle.check.spotless")
+    id("org.hiero.gradle.check.spotless-kotlin")
 }
 
-val maven = publishing.publications.create<MavenPublication>("maven") { from(components["java"]) }
-
-signing.sign(maven)
+dependencies { implementation(project(":pbj-grpc-helidon")) }

pbj-core/gradle/plugins/build.gradle.kts
@@ -1,32 +1,0 @@
-/*
- * Copyright (C) 2023-2024 Hedera Hashgraph, LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-plugins { `kotlin-dsl` }
-
-repositories { gradlePluginPortal() }
-
-dependencies {
-    implementation("com.adarshr:gradle-test-logger-plugin:4.0.0")
-    implementation("com.autonomousapps:dependency-analysis-gradle-plugin:1.29.0")
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.25.0")
-    implementation("com.google.protobuf:protobuf-gradle-plugin:0.9.4")
-    implementation("com.gradle.publish:plugin-publish-plugin:1.2.1")
-    implementation("gradle.plugin.lazy.zoo.gradle:git-data-plugin:1.2.2")
-    implementation("io.github.gradle-nexus:publish-plugin:1.3.0")
-    implementation("me.champeau.jmh:jmh-gradle-plugin:0.7.2")
-    implementation("net.swiftzer.semver:semver:1.3.0")
-    implementation("org.gradlex:java-module-dependencies:1.6.6")
-}

pbj-core/hiero-dependency-versions/build.gradle.kts
@@ -1,0 +1,89 @@
+/*
+ * Copyright (C) 2023-2025 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+    id("org.hiero.gradle.base.lifecycle")
+    id("org.hiero.gradle.base.jpms-modules")
+    id("org.hiero.gradle.check.spotless")
+    id("org.hiero.gradle.check.spotless-kotlin")
+}
+
+group = "com.hedera.hashgraph"
+
+val antlr = "4.11.1"
+val grpc = "1.65.1"
+val helidon = "4.1.6"
+val protobuf = "4.28.2"
+
+val junit5 = "5.8.2"
+val mockito = "5.10.0"
+
+dependencies.constraints {
+    api("org.antlr:antlr4-runtime:$antlr") { because("org.antlr.antlr4.runtime") }
+    api("com.github.spotbugs:spotbugs-annotations:4.8.6") {
+        because("com.github.spotbugs.annotations")
+    }
+
+    // The libs of this catalog are used to compile but are not bundled. The user will select
+    // helidon, which will have the set of these dependencies that are required.
+    api("io.helidon.webserver:helidon-webserver:$helidon") { because("io.helidon.webserver") }
+    api("io.helidon.webserver:helidon-webserver-http2:$helidon") {
+        because("io.helidon.webserver.http2")
+    }
+    api("io.helidon.metrics:helidon-metrics-api:$helidon") { because("io.helidon.metrics.api") }
+    api("io.helidon.common.features:helidon-common-features-api:$helidon") {
+        because("io.helidon.common.features.api")
+    }
+
+    // Annotation processing
+    api("io.helidon.common.features:helidon-common-features-processor:$helidon") {
+        because("io.helidon.common.features.processor")
+    }
+    api("io.helidon.config:helidon-config-metadata-processor:$helidon") {
+        because("io.helidon.config.metadata.processor")
+    }
+    api("io.helidon.codegen:helidon-codegen-apt:$helidon") { because("io.helidon.codegen.apt") }
+    api("io.helidon.builder:helidon-builder-codegen:$helidon") {
+        because("io.helidon.builder.codegen")
+    }
+
+    // Code generation
+    api("org.antlr:antlr4:$antlr")
+    api("com.google.protobuf:protoc:$protobuf")
+    api("io.grpc:protoc-gen-grpc-java:1.66.0")
+
+    // Testing only
+    api("com.google.guava:guava:33.3.1-jre") { because("com.google.common") }
+    api("com.google.protobuf:protobuf-java:$protobuf") { because("com.google.protobuf") }
+    api("com.google.protobuf:protobuf-java-util:$protobuf") { because("com.google.protobuf.util") }
+    api("org.assertj:assertj-core:3.23.1") { because("org.assertj.core") }
+    api("org.junit.jupiter:junit-jupiter-api:$junit5") { because("org.junit.jupiter.api") }
+    api("org.junit.jupiter:junit-jupiter-engine:$junit5") { because("org.junit.jupiter.engine") }
+
+    api("org.mockito:mockito-core:$mockito") { because("org.mockito") }
+    api("org.mockito:mockito-junit-jupiter:$mockito") { because("org.mockito.junit.jupiter") }
+    api("io.grpc:grpc-netty:$grpc") { because("io.grpc.netty") }
+    api("io.grpc:grpc-protobuf:$grpc") { because("io.grpc.protobuf") }
+    api("io.grpc:grpc-stub:$grpc") { because("io.grpc.stub") }
+
+    api("io.helidon.webserver.observe:helidon-webserver-observe-metrics:$helidon") {
+        because("io.helidon.webserver.observe.metrics")
+    }
+    api("io.helidon.webclient:helidon-webclient:$helidon") { because("io.helidon.webclient") }
+    api("io.helidon.webclient:helidon-webclient-http2:$helidon") {
+        because("io.helidon.webclient.http2")
+    }
+}

pbj-core/pbj-compiler/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022-2024 Hedera Hashgraph, LLC
+ * Copyright (C) 2022-2025 Hedera Hashgraph, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,15 @@
  * limitations under the License.
  */
 
-plugins { id("com.hedera.pbj.gradle-plugin") }
+plugins {
+    id("org.hiero.gradle.module.gradle-plugin")
+    id("org.hiero.gradle.feature.antlr")
+}
+
+description = "The PBJ Protobuf plugin provides protobuf compilation to java records."
+
+// Address warnings and remove this exception
+tasks.compileJava { options.compilerArgs.add("-Xlint:-text-blocks") }
 
 // This project does not have a module-info.java, as Gradle does not support plugins that are
 // Java Modules. For consistency, we still defined dependencies in terms of Module Names here.
@@ -36,10 +44,22 @@ gradlePlugin {
             group = "com.hedera.pbj"
             implementationClass = "com.hedera.pbj.compiler.PbjCompilerPlugin"
             displayName = "PBJ Compiler"
-            description = "The PBJ Protobuf plugin provides protobuf compilation to java records."
+            description = project.description
             website = "https://github.com/hashgraph/pbj"
             vcsUrl = "https://github.com/hashgraph/pbj"
             tags.set(listOf("protobuf", "compiler", "generator", "runtime"))
         }
     }
 }
+
+// Define package name for code generated by ANTLR
+tasks.generateGrammarSource {
+    arguments = arguments + listOf("-package", "com.hedera.pbj.compiler.impl.grammar")
+}
+
+// Do not generate Java Doc for generated antlr grammar
+tasks.javadoc { excludes.add("com/hedera/pbj/compiler/impl/grammar/**") }
+
+// As a Gradle plugin cannot be a Java Module, we do not have official internal packages.
+// We tell the dependency analysis to treat packages as "internal".
+dependencyAnalysis { abi { exclusions { ignoreSubPackage("impl") } } }

commit d05005913d911eb05d75f9c04737ee637a5f3b92

ci: re-enable integration tests on CI and align Java version on CI
- Make sure 'PBJ Integration' steps are no longer skipped
  (issue likely introduced in #302)
- Set Java version on CI to same version defined in
  'toolchain-versions.properties' files
- Fix exist quality check issues in pbj-integration-tests

Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

.github/workflows/flow-deploy-release-artifact.yaml
@@ -6,7 +6,7 @@ on:
         description: "Java JDK Version:"
         type: string
         required: false
-        default: "21.0.3"
+        default: "21.0.5"
       java-distribution:
         description: "Java JDK Distribution:"
         type: string
@@ -103,7 +103,7 @@ jobs:
         uses: actions/setup-java@7a6d8a8234af8eb26422e24e3006232cccaa061b # v4.6.0
         with:
           distribution: ${{ github.event.inputs.java-distribution || 'temurin' }}
-          java-version: ${{ github.event.inputs.java-version || '21.0.3' }}
+          java-version: ${{ github.event.inputs.java-version || '21.0.5' }}
 
       - name: Setup Gradle
         uses: gradle/actions/setup-gradle@0bdd871935719febd78681f197cd39af5b6e16a6 # v4.2.2

.github/workflows/zxc-compile-pbj-code.yaml
@@ -26,7 +26,7 @@ on:
         description: "Java JDK Version:"
         type: string
         required: false
-        default: "21.0.3"
+        default: "21.0.5"
       gradle-version:
         description: "Gradle Version:"
         type: string
@@ -122,7 +122,7 @@ jobs:
 
       - name: Gradle Assemble (PBJ Integration)
         id: gradle-integration-build
-        if: ${{ (inputs.enable-integration-tests || inputs.enable-jmh-tests) && steps.gradle-publish-local.conclusion == 'success' && !cancelled() }}
+        if: ${{ (inputs.enable-integration-tests || inputs.enable-jmh-tests) && !cancelled() }}
         working-directory: ${{ env.PBJ_INTEGRATION_TESTS }}
         run: ./gradlew assemble
 

pbj-integration-tests/build.gradle.kts
@@ -27,7 +27,6 @@ mainModuleInfo {
 
     requires("com.google.common")
     requires("com.google.protobuf")
-    requires("com.google.protobuf.util")
     requires("io.grpc")
     requires("io.grpc.protobuf")
     requires("io.grpc.stub")
@@ -38,10 +37,14 @@ mainModuleInfo {
 testModuleInfo {
     requires("org.junit.jupiter.api")
     requires("org.junit.jupiter.params")
+    requires("com.google.protobuf.util")
     runtimeOnly("org.junit.jupiter.engine")
 }
 
-jmhModuleInfo { requires("com.hedera.pbj.runtime") }
+jmhModuleInfo {
+    requires("com.hedera.pbj.runtime")
+    requires("com.google.protobuf.util")
+}
 
 // IMPROVE: Disable module-info transform for 'testRuntimeClasspath' which leads to an error
 // possible caused by a cycle produced by depending on 'pbj-compiler' in multiple ways which