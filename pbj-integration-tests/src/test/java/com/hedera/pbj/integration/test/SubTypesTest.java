394436a1c2e56e0e650021fd291ddc4513c0c9b2
Anthony Petrov <anthony@swirldslabs.com>
2025-03-11 15:02:11 -0700

    fix: handle partially-qualified inner message names
    Signed-off-by: Anthony Petrov <anthony@swirldslabs.com>

---
+++ pbj-integration-tests/src/test/java/com/hedera/pbj/integration/test/SubTypesTest.java
@@ -0,0 +1,39 @@
+package com.hedera.pbj.integration.test;
+
+import static com.hedera.pbj.compiler.PbjCompiler.compileFilesIn;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+
+public class SubTypesTest {
+    @TempDir
+    private static File outputDir;
+
+    @Test
+    void testSubTypes() throws Exception {
+        // Verify if we're able to reference inner and imported types w/o using fully qualified names
+        getCompileFilesIn("sub_types.proto", "sub_types_import.proto");
+    }
+
+    private static void getCompileFilesIn(String... fileNames) throws Exception {
+        final List<File> files = Arrays.stream(fileNames)
+                .map(fileName -> {
+                    try {
+                        final URL url = SubTypesTest.class.getClassLoader().getResource(fileName);
+                        final URI uri = url.toURI();
+                        return new File(uri);
+                    } catch (URISyntaxException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .toList();
+        compileFilesIn(files, outputDir, outputDir);
+    }
+}
