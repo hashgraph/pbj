52406feb07c11bac663ed5ffd40ba6eebcfbadce
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-06-26 14:23:08 +0200

    build: change publishing to use maven central publishing portal (#511)
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>
    Signed-off-by: Roger Barker <roger.barker@swirldslabs.com>
    Co-authored-by: Roger Barker <roger.barker@swirldslabs.com>

--- pbj-core/gradle/aggregation/build.gradle.kts
+++ pbj-core/gradle/aggregation/build.gradle.kts
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: Apache-2.0
 plugins {
-    id("org.hiero.gradle.base.lifecycle")
+    id("org.hiero.gradle.feature.publish-maven-central-aggregation")
     id("org.hiero.gradle.report.code-coverage")
     id("org.hiero.gradle.check.spotless")
     id("org.hiero.gradle.check.spotless-kotlin")
 }
 
 dependencies {
-    implementation(project(":pbj-grpc-helidon"))
-    implementation(project(":pbj-grpc-client-helidon"))
+    published(project(":pbj-grpc-helidon"))
+    published(project(":pbj-grpc-client-helidon"))
 }
