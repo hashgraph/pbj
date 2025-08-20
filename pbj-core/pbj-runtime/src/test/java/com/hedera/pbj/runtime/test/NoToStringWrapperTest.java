061f9df9d61304e8d04fa3772a24d642d8f0b57c
Josh Marinacci <joshua@marinacci.org>
2025-08-20 16:14:24 -0700

    add tests for NoToStringWrapper
    Signed-off-by: Josh Marinacci <joshua@marinacci.org>

---
+++ pbj-core/pbj-runtime/src/test/java/com/hedera/pbj/runtime/test/NoToStringWrapperTest.java
@@ -0,0 +1,35 @@
+package com.hedera.pbj.runtime.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NoToStringWrapperTest {
+    @Test
+    void wrapString() {
+        var foo = "foo".intern();
+        var wrapper = new NoToStringWrapper<String>(foo);
+        //get the wrapped value
+        assertEquals("foo",wrapper.getValue());
+        assertEquals("NoToStringWrapper{java.lang.String}",wrapper.toString());
+        assertTrue(wrapper.equals(wrapper));
+        assertTrue(wrapper.equals(new NoToStringWrapper<String>("foo")));
+        assertFalse(wrapper.equals(new NoToStringWrapper<String>("bar")));
+        assertEquals("foo".hashCode(),"foo".hashCode());
+        assertEquals(wrapper.hashCode(),foo.hashCode());
+    }
+
+    @Test
+    void wrapFloat() {
+        var wrapper = new NoToStringWrapper<Float>(8.8f);
+        assertEquals(8.8f,wrapper.getValue());
+        assertEquals("NoToStringWrapper{java.lang.Float}",wrapper.toString());
+        assertTrue(wrapper.equals(wrapper));
+        assertTrue(wrapper.equals(new NoToStringWrapper<Float>(8.8f)));
+        assertFalse(wrapper.equals(new NoToStringWrapper<Float>(9.9f)));
+        assertEquals(Float.valueOf(8.8f).hashCode(),Float.valueOf(8.8f).hashCode());
+        assertEquals(wrapper.hashCode(),Float.valueOf(8.8f).hashCode());
+    }
+}
