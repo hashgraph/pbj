87696c202aa982edaadef023dc26693ff06a4e5a
Josh Marinacci <joshua@marinacci.org>
2025-08-21 10:06:26 -0700

    spotless reformatting
    Signed-off-by: Josh Marinacci <joshua@marinacci.org>

--- pbj-core/pbj-runtime/src/test/java/com/hedera/pbj/runtime/test/CharBufferToWritableSequentialDataTest.java
+++ pbj-core/pbj-runtime/src/test/java/com/hedera/pbj/runtime/test/CharBufferToWritableSequentialDataTest.java
@@ -1,11 +1,11 @@
+// SPDX-License-Identifier: Apache-2.0
 package com.hedera.pbj.runtime.test;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 import java.nio.CharBuffer;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 class CharBufferToWritableSequentialDataTest {
     private static final int CHAR_BUFFER_SIZE = 16 * 1024 * 1024;
@@ -22,13 +22,12 @@ class CharBufferToWritableSequentialDataTest {
     void basicWriting() throws IOException {
         final var charBuffer = getThreadLocalCharBuffer();
         final CharBufferToWritableSequentialData adapter = new CharBufferToWritableSequentialData(charBuffer);
-        assertEquals(CHAR_BUFFER_SIZE,adapter.capacity());
-        assertEquals(0,adapter.position());
+        assertEquals(CHAR_BUFFER_SIZE, adapter.capacity());
+        assertEquals(0, adapter.position());
         adapter.writeUTF8("foo");
-        assertEquals(CHAR_BUFFER_SIZE,adapter.capacity());
-        assertEquals(3,adapter.position());
+        assertEquals(CHAR_BUFFER_SIZE, adapter.capacity());
+        assertEquals(3, adapter.position());
         adapter.skip(20);
-        assertEquals(23,adapter.position());
+        assertEquals(23, adapter.position());
     }
-
 }
--- pbj-core/pbj-runtime/src/test/java/com/hedera/pbj/runtime/test/NoToStringWrapperTest.java
+++ pbj-core/pbj-runtime/src/test/java/com/hedera/pbj/runtime/test/NoToStringWrapperTest.java
@@ -1,37 +1,38 @@
+// SPDX-License-Identifier: Apache-2.0
 package com.hedera.pbj.runtime.test;
-
-import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 public class NoToStringWrapperTest {
     @Test
     void wrapString() {
         var foo = "foo".intern();
         var wrapper = new NoToStringWrapper<String>(foo);
-        //get the wrapped value
-        assertEquals("foo",wrapper.getValue());
-        assertEquals("NoToStringWrapper{java.lang.String}",wrapper.toString());
+        // get the wrapped value
+        assertEquals("foo", wrapper.getValue());
+        assertEquals("NoToStringWrapper{java.lang.String}", wrapper.toString());
         assertTrue(wrapper.equals(wrapper));
         assertTrue(wrapper.equals(new NoToStringWrapper<String>("foo")));
         assertFalse(wrapper.equals(new NoToStringWrapper<String>("bar")));
-        assertEquals("foo".hashCode(),"foo".hashCode());
-        assertEquals(wrapper.hashCode(),foo.hashCode());
+        assertEquals("foo".hashCode(), "foo".hashCode());
+        assertEquals(wrapper.hashCode(), foo.hashCode());
         assertFalse(wrapper.equals(null));
     }
 
     @Test
     void wrapFloat() {
         var wrapper = new NoToStringWrapper<Float>(8.8f);
-        assertEquals(8.8f,wrapper.getValue());
-        assertEquals("NoToStringWrapper{java.lang.Float}",wrapper.toString());
+        assertEquals(8.8f, wrapper.getValue());
+        assertEquals("NoToStringWrapper{java.lang.Float}", wrapper.toString());
         assertTrue(wrapper.equals(wrapper));
         assertTrue(wrapper.equals(new NoToStringWrapper<Float>(8.8f)));
         assertFalse(wrapper.equals(new NoToStringWrapper<Float>(9.9f)));
-        assertEquals(Float.valueOf(8.8f).hashCode(),Float.valueOf(8.8f).hashCode());
-        assertEquals(wrapper.hashCode(),Float.valueOf(8.8f).hashCode());
+        assertEquals(Float.valueOf(8.8f).hashCode(), Float.valueOf(8.8f).hashCode());
+        assertEquals(wrapper.hashCode(), Float.valueOf(8.8f).hashCode());
         assertFalse(wrapper.equals(new NoToStringWrapper<String>("foo")));
         assertFalse(wrapper.equals("foo"));
     }
