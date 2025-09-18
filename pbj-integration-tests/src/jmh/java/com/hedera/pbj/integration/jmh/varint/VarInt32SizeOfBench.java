278d3a1eb65d542501866a4eac1add7cc75b256e
Anthony Petrov <anthony@swirldslabs.com>
2025-09-17 17:24:50 -0700

    perf: improve varint writing performance
    Signed-off-by: Anthony Petrov <anthony@swirldslabs.com>

---
+++ pbj-integration-tests/src/jmh/java/com/hedera/pbj/integration/jmh/varint/VarInt32SizeOfBench.java
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: Apache-2.0
+package com.hedera.pbj.integration.jmh.varint;
+
+import com.hedera.pbj.runtime.ProtoWriterTools;
+import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.CompilerControl;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OperationsPerInvocation;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+@SuppressWarnings("unused")
+@State(Scope.Benchmark)
+@Fork(1)
+@Warmup(iterations = 3, time = 2)
+@Measurement(iterations = 4, time = 2)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@BenchmarkMode(Mode.AverageTime)
+public class VarInt32SizeOfBench {
+    public static final int NUM_OF_VALUES = 1024;
+
+    @Param({"1", "2", "4"})
+    public int numOfBytes;
+
+    private int[] numbers;
+
+    @Setup
+    public void setupNumbers() {
+        Random random = new Random(9387498731984L);
+        numbers = new int[NUM_OF_VALUES];
+        final int minValue = numOfBytes == 1 ? 0 : 1 << ((numOfBytes - 1) * 7);
+        final int maxValue = (1 << (numOfBytes * 7)) - 1;
+        for (int i = 0; i < NUM_OF_VALUES; i++) {
+            this.numbers[i] = random.nextInt(minValue, maxValue);
+        }
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    @OperationsPerInvocation(NUM_OF_VALUES)
+    public void pbjProtoTools(Blackhole blackhole) {
+        for (int i = 0; i < NUM_OF_VALUES; i++) blackhole.consume(ProtoWriterTools.sizeOfUnsignedVarInt32(numbers[i]));
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    @OperationsPerInvocation(NUM_OF_VALUES)
+    public void kafka(Blackhole blackhole) {
+        for (int i = 0; i < NUM_OF_VALUES; i++) blackhole.consume(kafkaSizeOfUnsignedVarint(numbers[i]));
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    @OperationsPerInvocation(NUM_OF_VALUES)
+    public void google(Blackhole blackhole) throws IOException {
+        for (int i = 0; i < NUM_OF_VALUES; i++)
+            blackhole.consume(com.google.protobuf.CodedOutputStream.computeInt32SizeNoTag(numbers[i]));
+    }
+
+    public static int kafkaSizeOfUnsignedVarint(int value) {
+        // Protocol buffers varint encoding is variable length, with a minimum of 1 byte
+        // (for zero). The values themselves are not important. What's important here is
+        // any leading zero bits are dropped from output. We can use this leading zero
+        // count w/ fast intrinsic to calc the output length directly.
+        // Test cases verify this matches the output for loop logic exactly.
+        // return (38 - leadingZeros) / 7 + leadingZeros / 32;
+        // The above formula provides the implementation, but the Java encoding is suboptimal
+        // when we have a narrow range of integers, so we can do better manually
+        int leadingZeros = Integer.numberOfLeadingZeros(value);
+        int leadingZerosBelow38DividedBy7 = ((38 - leadingZeros) * 0b10010010010010011) >>> 19;
+        return leadingZerosBelow38DividedBy7 + (leadingZeros >>> 5);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Options opt = new OptionsBuilder()
+                .include(VarInt32SizeOfBench.class.getSimpleName())
+                .build();
+
+        new Runner(opt).run();
+    }
+}
---
+++ pbj-integration-tests/src/jmh/java/com/hedera/pbj/integration/jmh/varint/VarIntWriterBench.java
@@ -0,0 +1,134 @@
+// SPDX-License-Identifier: Apache-2.0
+package com.hedera.pbj.integration.jmh.varint;
+
+import com.hedera.pbj.integration.jmh.varint.writers.GoogleCodedByteArray;
+import com.hedera.pbj.integration.jmh.varint.writers.GoogleCodedByteBufferDirect;
+import com.hedera.pbj.integration.jmh.varint.writers.GoogleCodedOutputStream;
+import com.hedera.pbj.integration.jmh.varint.writers.KafkaByteBuffer;
+import com.hedera.pbj.integration.jmh.varint.writers.PbjBufferedData;
+import com.hedera.pbj.integration.jmh.varint.writers.PbjBufferedDataDirect;
+import com.hedera.pbj.integration.jmh.varint.writers.PbjWritableStreamingData;
+import com.hedera.pbj.integration.jmh.varint.writers.RichardStartinByteArray;
+import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.CompilerControl;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OperationsPerInvocation;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+@SuppressWarnings("unused")
+@State(Scope.Benchmark)
+@Fork(1)
+@Warmup(iterations = 4, time = 2)
+@Measurement(iterations = 5, time = 2)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@BenchmarkMode(Mode.AverageTime)
+public class VarIntWriterBench {
+    public static final int NUM_OF_VALUES = 4096 * 4;
+    /**
+     * Number of bytes to read at a time (1, 2, 4, or 8). So create inputs with 1 byte siz,e, 2 byte size, 4 byte size,
+     * and 8 byte size.
+     */
+    //    @Param({"1", "2", "3", "4", "8"})
+    @Param({"4"})
+    public int numOfBytes;
+
+    private long[] numbers;
+
+    @Setup
+    public void setupNumbers() {
+        Random random = new Random(9387498731984L);
+        numbers = new long[NUM_OF_VALUES];
+        final long minValue = numOfBytes == 1 ? 0L : 1L << ((numOfBytes - 1) * 7);
+        final long maxValue = (1L << (numOfBytes * 7)) - 1;
+        // System.out.println("Generating "+NUM_OF_VALUES+" random numbers between "+min
+        for (int i = 0; i < NUM_OF_VALUES; i++) {
+            this.numbers[i] = random.nextLong(minValue, maxValue);
+        }
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    @OperationsPerInvocation(NUM_OF_VALUES)
+    public void googleCodedOutputStream(GoogleCodedOutputStream state) throws IOException {
+        for (int i = 0; i < NUM_OF_VALUES; i++) state.writeVarint(numbers[i]);
+        state.endLoop();
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    @OperationsPerInvocation(NUM_OF_VALUES)
+    public void googleCodedByteArray(GoogleCodedByteArray state) throws IOException {
+        for (int i = 0; i < NUM_OF_VALUES; i++) state.writeVarint(numbers[i]);
+        state.endLoop();
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    @OperationsPerInvocation(NUM_OF_VALUES)
+    public void googleCodedByteBufferDirect(GoogleCodedByteBufferDirect state) throws IOException {
+        for (int i = 0; i < NUM_OF_VALUES; i++) state.writeVarint(numbers[i]);
+        state.endLoop();
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    @OperationsPerInvocation(NUM_OF_VALUES)
+    public void pbjWritableStreamingData(PbjWritableStreamingData state) throws IOException {
+        for (int i = 0; i < NUM_OF_VALUES; i++) state.writeVarint(numbers[i]);
+        state.endLoop();
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    @OperationsPerInvocation(NUM_OF_VALUES)
+    public void pbjBufferedData(PbjBufferedData state) throws IOException {
+        for (int i = 0; i < NUM_OF_VALUES; i++) state.writeVarint(numbers[i]);
+        state.endLoop();
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    @OperationsPerInvocation(NUM_OF_VALUES)
+    public void pbjBufferedDataDirect(PbjBufferedDataDirect state) throws IOException {
+        for (int i = 0; i < NUM_OF_VALUES; i++) state.writeVarint(numbers[i]);
+        state.endLoop();
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    @OperationsPerInvocation(NUM_OF_VALUES)
+    public void richardStartinByteArray(RichardStartinByteArray state) throws IOException {
+        for (int i = 0; i < NUM_OF_VALUES; i++) state.writeVarint(numbers[i]);
+        state.endLoop();
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    @OperationsPerInvocation(NUM_OF_VALUES)
+    public void kafkaByteBuffer(KafkaByteBuffer state) throws IOException {
+        for (int i = 0; i < NUM_OF_VALUES; i++) state.writeVarint(numbers[i]);
+        state.endLoop();
+    }
+
+    public static void main(String[] args) throws Exception {
+        Options opt = new OptionsBuilder()
+                .include(VarIntWriterBench.class.getSimpleName())
+                .build();
+
+        new Runner(opt).run();
+    }
+}
