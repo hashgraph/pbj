a93d2720083e5874c40903d83f536e7a385d514a
Anthony Petrov <anthony@swirldslabs.com>
2025-10-07 16:43:57 -0700

    feat: introduce WritableMessageDigest
    Signed-off-by: Anthony Petrov <anthony@swirldslabs.com>

---
+++ pbj-core/pbj-runtime/src/main/java/com/hedera/pbj/runtime/hashing/WritableMessageDigest.java
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: Apache-2.0
+package com.hedera.pbj.runtime.hashing;
+
+import com.hedera.pbj.runtime.io.WritableSequentialData;
+import com.hedera.pbj.runtime.io.buffer.BufferedData;
+import com.hedera.pbj.runtime.io.buffer.RandomAccessData;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import java.io.UncheckedIOException;
+import java.nio.BufferOverflowException;
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.util.Objects;
+
+/**
+ * A WritableSequentialData implementation that writes all the data into a given MessageDigest instance.
+ */
+public class WritableMessageDigest implements WritableSequentialData {
+    private final MessageDigest digest;
+
+    /**
+     * Constructor.
+     * @param digest a MessageDigest object to write data to
+     */
+    public WritableMessageDigest(@NonNull final MessageDigest digest) {
+        this.digest = Objects.requireNonNull(digest);
+    }
+
+    @Override
+    public void writeByte(byte b) throws BufferOverflowException, UncheckedIOException {
+        digest.update(b);
+    }
+
+    @Override
+    public void writeBytes(@NonNull byte[] src) throws BufferOverflowException, UncheckedIOException {
+        digest.update(src);
+    }
+
+    @Override
+    public void writeBytes(@NonNull byte[] src, int offset, int length)
+            throws BufferOverflowException, UncheckedIOException {
+        digest.update(src, offset, length);
+    }
+
+    @Override
+    public void writeBytes(@NonNull ByteBuffer src) throws BufferOverflowException, UncheckedIOException {
+        digest.update(src);
+    }
+
+    @Override
+    public void writeBytes(@NonNull BufferedData src) throws BufferOverflowException, UncheckedIOException {
+        src.writeTo(digest);
+    }
+
+    @Override
+    public void writeBytes(@NonNull RandomAccessData src) throws BufferOverflowException, UncheckedIOException {
+        src.writeTo(digest);
+    }
+
+    /**
+     * This WritableSequentialData implementation feeds an underlying MessageDigest object, so there's not a concept
+     * of a capacity here, and this method always returns Long.MAX_VALUE.
+     * @return Long.MAX_VALUE
+     */
+    @Override
+    public long capacity() {
+        return Long.MAX_VALUE;
+    }
+
+    /**
+     * This WritableSequentialData implementation feeds an underlying MessageDigest object, so there's not a concept
+     * of a position here, and this method always returns zero.
+     * @return zero
+     */
+    @Override
+    public long position() {
+        return 0;
+    }
+
+    /**
+     * This WritableSequentialData implementation feeds an underlying MessageDigest object, so there's not a concept
+     * of a limit here, and this method always returns Long.MAX_VALUE.
+     * @return Long.MAX_VALUE
+     */
+    @Override
+    public long limit() {
+        return Long.MAX_VALUE;
+    }
+
+    /**
+     * This WritableSequentialData implementation feeds an underlying MessageDigest object, so there's not a concept
+     * of a limit here, and this method is a no-op.
+     */
+    @Override
+    public void limit(long limit) {}
+
+    /**
+     * This WritableSequentialData implementation feeds an underlying MessageDigest object, so there's not a concept
+     * of a position here, and this method is a no-op.
+     */
+    @Override
+    public void skip(long count) throws UncheckedIOException {}
+}
