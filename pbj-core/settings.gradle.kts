d9001f042882008ed0e5b7a66598daa1a33a65ee
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-10-24 12:58:40 +0200

    use 0.6.0 final release
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

--- pbj-core/settings.gradle.kts
+++ pbj-core/settings.gradle.kts
@@ -1,16 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        maven("https://central.sonatype.com/repository/maven-snapshots")
-    }
-}
-
-buildscript {
-    configurations.classpath { resolutionStrategy.cacheDynamicVersionsFor(0, "seconds") }
-}
-
-plugins { id("org.hiero.gradle.build") version "0.6.0-SNAPSHOT" }
+plugins { id("org.hiero.gradle.build") version "0.6.0" }
 
 javaModules {
     directory(".") {
--- pbj-integration-tests/settings.gradle.kts
+++ pbj-integration-tests/settings.gradle.kts
@@ -1,17 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        maven("https://central.sonatype.com/repository/maven-snapshots")
-        includeBuild("../pbj-core") // use locally built 'pbj-core' (Gradle plugin)
-    }
+    includeBuild("../pbj-core") // use locally built 'pbj-core' (Gradle plugin)
 }
 
-buildscript {
-    configurations.classpath { resolutionStrategy.cacheDynamicVersionsFor(0, "seconds") }
-}
-
-plugins { id("org.hiero.gradle.build") version "0.6.0-SNAPSHOT" }
+plugins { id("org.hiero.gradle.build") version "0.6.0" }
 
 dependencyResolutionManagement {
     // To use locally built 'pbj-runtime'
