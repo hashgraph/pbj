78c4802614fdee802cda071fe01505f38ae67945
Jendrik Johannes <jendrik@onepiece.software>
2026-05-07 16:52:19 +0200

    build: annotated PbjCompilerTask as Cacheable
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>
    Signed-off-by: Jendrik Johannes <jendrik@onepiece.software>

--- pbj-core/pbj-compiler/src/main/java/com/hedera/pbj/compiler/PbjCompilerTask.java
+++ pbj-core/pbj-compiler/src/main/java/com/hedera/pbj/compiler/PbjCompilerTask.java
@@ -9,6 +9,7 @@ import org.gradle.api.file.DirectoryProperty;
 import org.gradle.api.internal.file.FileOperations;
 import org.gradle.api.provider.Property;
 import org.gradle.api.provider.SetProperty;
+import org.gradle.api.tasks.CacheableTask;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputFiles;
 import org.gradle.api.tasks.Internal;
@@ -20,6 +21,7 @@ import org.gradle.api.tasks.SourceTask;
 import org.gradle.api.tasks.TaskAction;
 
 /** Gradle Task that generates java src code from protobuf proto schema files. */
+@CacheableTask
 public abstract class PbjCompilerTask extends SourceTask {
 
     /**
