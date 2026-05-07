54ff948b487b5ccd9ae04406b5618ed3353aed4b
Jendrik Johannes <jendrik@onepiece.software>
2026-05-07 16:42:07 +0200

    build: remove rules now shipped with plugins
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>
    Signed-off-by: Jendrik Johannes <jendrik@onepiece.software>

--- pbj-core/settings.gradle.kts
+++ pbj-core/settings.gradle.kts
@@ -7,20 +7,3 @@ javaModules {
         module("pbj-compiler") // no 'module-info.java'
     }
 }
-
-// The patch rules below can be removed once "org.hiero.gradle.build" contains the following update:
-// https://github.com/hiero-ledger/hiero-gradle-conventions/issues/444
-@Suppress("UnstableApiUsage")
-gradle.lifecycle.beforeProject {
-    plugins.withId("org.hiero.gradle.base.jpms-modules") {
-        configure<JvmDependencyConflictsExtension> {
-            patch.module("io.prometheus:simpleclient") {
-                addRuntimeOnlyDependency("io.prometheus:simpleclient_tracer_common")
-            }
-        }
-        configure<ExtraJavaModuleInfoPluginExtension> {
-            module("io.micrometer:micrometer-registry-otlp", "micrometer.registry.otlp")
-            module("io.opentelemetry.proto:opentelemetry-proto", "io.opentelemetry.proto")
-        }
-    }
-}
